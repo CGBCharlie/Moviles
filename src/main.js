b707231c4ef1e88d4a556feaa9a4ab8dc04253bc
CGBCharlie <carlitosgarcia2000@gmail.com>
2022-05-17 08:53:15 -0500

    Firebase claves database

--- src/main.js
+++ src/main.js
@@ -23,9 +23,30 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvgkZxapIJBZJgiIz9H99kuHrDwoY5NTA",
+  authDomain: "practica-ec284.firebaseapp.com",
+  databaseURL: "https://practica-ec284-default-rtdb.firebaseio.com",
+  projectId: "practica-ec284",
+  storageBucket: "practica-ec284.appspot.com",
+  messagingSenderId: "722977346394",
+  appId: "1:722977346394:web:9388136554ff011f2b5963"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(appFirebase)
+  ;
   
 router.isReady().then(() => {
   app.mount('#app');
